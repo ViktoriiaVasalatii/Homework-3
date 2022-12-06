92896f52acf67d08de2495a5fc8abfc37ab47aed
Viktoriia <vacalatyvika02@gmail.com>
2022-12-06 14:20:52 +0200

    fixed errors

--- Hour of second/script.js
+++ Hour of second/script.js
@@ -1,3 +1,3 @@
-numberHour = +prompt("Enter number of hour");
+const numberHour = +prompt("Enter number of hour");
 let result = numberHour * 60 * 60 ;
 alert(result);
--- Mathematical operations/script.js
+++ Mathematical operations/script.js
@@ -1,12 +1,13 @@
-let firstNumber = +prompt("Enter the first number");
-let secondNumber = +prompt("Enter the second number");
+const firstNumber = +prompt("Enter the first number");
+const secondNumber = +prompt("Enter the second number");
 
 let resultAdd = firstNumber + secondNumber;
 let resultSub = firstNumber - secondNumber;
 let resultMul = firstNumber * secondNumber; 
 let resultDiv = firstNumber / secondNumber; 
 
-alert(`${firstNumber} + ${secondNumber} = ${resultAdd}`); 
-alert(`${firstNumber} - ${secondNumber} = ${resultSub}`); 
-alert(`${firstNumber} * ${secondNumber} = ${resultMul}`); 
-alert(`${firstNumber} / ${secondNumber} = ${resultDiv}`); 
+alert(`${firstNumber} + ${secondNumber} = ${resultAdd} 
+${firstNumber} - ${secondNumber} = ${resultSub}
+${firstNumber} * ${secondNumber} = ${resultMul}
+${firstNumber} / ${secondNumber} = ${resultDiv}`); 
+
